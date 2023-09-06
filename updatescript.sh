f14ae24f169000c3edf4af3b2bcc161f130193f6
503422student <503422@student.fontys.nl>
2023-09-06 14:19:25 +0200

    Detection fixes

--- updatescript.sh
+++ updatescript.sh
@@ -8,13 +8,16 @@ output=$(hostnamectl)
 os_line=$(echo "$output" | grep "Operating System")
 
 # Extract the OS name and version using awk and remove the LTS information
-os_info=$(echo "$os_line" | awk -F ': ' '{print $2}' | cut -d ' ' -f1,2)
+os_info=$(echo "$os_line" | awk -F ': ' '{print $2}' | sed 's/LTS//')
+
+# Extract the OS name without the version information
+os_name=$(echo "$os_info" | awk '{print $1}')
 
 # Convert the OS name to lowercase for case-insensitive comparison
-os_name=$(echo "$os_info" | awk '{print tolower($0)}')
+os_name_lowercase=$(echo "$os_name" | tr '[:upper:]' '[:lower:]')
 
 # Check if the OS info was found
-if [ -n "$os_name" ]; then
+if [ -n "$os_name_lowercase" ]; then
     echo "Operating System: $os_info"
 else
     echo "Operating System not found."
@@ -22,20 +25,20 @@ else
 fi
 
 # Check if the OS name is "ubuntu" (case-insensitive comparison)
-if [ "$os_name" == "ubuntu" ]; then
+if [ "$os_name_lowercase" == "ubuntu" ]; then
     echo "Detected Ubuntu, running apt update and upgrade commands"
     apt update
     apt upgrade -y
 fi
 
 # Check if the OS name is "fedora" (case-insensitive comparison)
-if [ "$os_name" == "fedora" ]; then
+if [ "$os_name_lowercase" == "fedora" ]; then
     echo "Detected Fedora, running dnf commands"
     dnf upgrade -y
 fi
 
 # Check if the OS name is "centos" (case-insensitive comparison)
-if [ "$os_name" == "centos" ]; then
+if [ "$os_name_lowercase" == "centos" ]; then
     centos_version=$(echo "$os_info" | awk '{print $2}' | cut -d '.' -f1)
     
     if [ "$centos_version" -le 7 ]; then
